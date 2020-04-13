9a1bc7608610020410b3374c6dd4add0b0c8fa1a
Rishabh Malhotra <Rishabhmalhotraa01@gmail.com>
2020-04-13 13:12:33 +0530

    Updates

---
+++ precache-manifest.fcdeeebf8499200fdad297b888268b91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6076e881ece052d5e8e753432e274b7",
+    "url": "/joplin-tables/index.html"
+  },
+  {
+    "revision": "48787d1f430527d826d5",
+    "url": "/joplin-tables/static/css/main.5c70208b.chunk.css"
+  },
+  {
+    "revision": "da5f6edf2a3e55764439",
+    "url": "/joplin-tables/static/js/2.07984dc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/joplin-tables/static/js/2.07984dc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48787d1f430527d826d5",
+    "url": "/joplin-tables/static/js/main.0e9ebfa7.chunk.js"
+  },
+  {
+    "revision": "3090e3a0aaf90f7d84a4",
+    "url": "/joplin-tables/static/js/runtime-main.39e13116.js"
+  }
+]);
